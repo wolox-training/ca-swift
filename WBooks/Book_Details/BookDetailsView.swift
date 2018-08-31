5078a2d3c7e775aecf7468079124b529758d3542
Carolina Arcos P <carolinaarcos@Woloxs-MacBook-Pro-6.local>
2018-08-31 10:12:02 -0500

    Add book details view

---
+++ WBooks/Book_Details/BookDetailsView.swift
@@ -0,0 +1,23 @@
+//
+//  BookDetailsView.swift
+//  WBooks
+//
+//  Created by Carolina Arcos on 8/30/18.
+//  Copyright © 2018 Wolox. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailsView: UIView {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var releaseYearLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var addToWishlistButton: UIButton!
+    @IBOutlet weak var rentButton: UIButton!
+}
