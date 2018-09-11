570654248815cd439f2168d07452d3295653a76a
Carolina Arcos P <carolinaarcos@Woloxs-MacBook-Pro-6.local>
2018-09-11 08:23:16 -0500

    Add LineTextField class

---
+++ WBooks/Helpers/LineTextField.swift
@@ -0,0 +1,13 @@
+//
+//  LineTextField.swift
+//  WBooks
+//
+//  Created by Carolina Arcos on 9/10/18.
+//  Copyright © 2018 Wolox. All rights reserved.
+//
+
+import UIKit
+
+class LineTextField: UITextField {
+    
+}
