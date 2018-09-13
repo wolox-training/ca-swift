70293f9faae24fbd4c5c0415f1f23297b6ac4777
Carolina Arcos P <carolinaarcos@Woloxs-MacBook-Pro-6.local>
2018-09-13 10:44:06 -0500

    Code review fixes

--- WBooks/Helpers/LineTextField.swift
+++ WBooks/Helpers/LineTextField.swift
@@ -10,18 +10,24 @@ import UIKit
 
 class LineTextField: UITextField {
     
+    override var placeholder: String? {
+        didSet {
+            configurePlaceholder()
+        }
+    }
+    
     // MARK: - Initializers
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
         configureTextField()
-        createBorder()
+        _ = add(bottom: .init(thickness: 0.5, color: GeneralConstants.Design.textfieldGrayColor))
     }
     
     required override init(frame: CGRect) {
         super.init(frame: frame)
         configureTextField()
-        createBorder()
+        _ = add(bottom: .init(thickness: 0.5, color: GeneralConstants.Design.textfieldGrayColor))
     }
     
     // MARK: - Private methods
@@ -32,21 +38,15 @@ class LineTextField: UITextField {
         font = GeneralConstants.Design.textFont
         textColor = UIColor.darkGray
         
+        configurePlaceholder()
+    }
+    
+    private func configurePlaceholder() {
         if let placeholder = placeholder {
-            attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [
-                NSAttributedStringKey.font: GeneralConstants.Design.textFont,
+            attributedPlaceholder = NSAttributedString(string: placeholder.capitalized, attributes: [
+                NSAttributedStringKey.font: GeneralConstants.Design.placeholderFont,
                 NSAttributedStringKey.foregroundColor: GeneralConstants.Design.textfieldGrayColor
                 ])
         }
-    }
-    
-    private func createBorder() {
-        let border = CALayer()
-        let width = CGFloat(1.0)
-        border.borderColor = GeneralConstants.Design.textfieldGrayColor.cgColor
-        border.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: frame.size.width, height: frame.size.height)
-        border.borderWidth = width
-        self.layer.addSublayer(border)
-        self.layer.masksToBounds = true
     }
 }
